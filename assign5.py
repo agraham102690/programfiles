a76819920fcc4cdf69263dfd9e5ac36a80c675a3
agraham102690 <grahamam1026@gmail.com>
2022-11-25 13:10:17 -0500

    start of the program

---
+++ assign5.py
@@ -0,0 +1,13 @@
+user_names = []
+eye_color = ("green", "blue", "brown", "hazel", "grey")
+print("Eye colors: " + eye_color)
+hair_color = ("brown", "black", "blonde", "red" )
+print("Hair colors: " + hair_color)
+city_state = {"City":"State"}
+def user_profile():
+    user_name_question = input("Enter a username: ")
+    user_name_question.append(user_names)
+    eye_color_question = input("What color are your eyes? : ")
+    hair_color_qusestion = input("What color is your hair")
+    city_state_question = input("What city and state do you live in? :")
+
