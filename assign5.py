a0f60a6d58151be403e641001764e23c69620c09
agraham102690 <grahamam1026@gmail.com>
2022-11-25 14:29:48 -0500

    changed user profile to usernamedata

--- assign5.py
+++ assign5.py
@@ -3,7 +3,7 @@ eye_color = ("green", "blue", "brown", "hazel", "grey")
 print("Eye colors: " + str(eye_color))
 hair_color = ("brown", "black", "blonde", "red" )
 print("Hair colors: " + str(hair_color))
-city_state = {"":""}
+city_state = {"City":"State"}
 while True:
 
     user_name_question = input("Enter a username: ")
@@ -11,10 +11,11 @@ while True:
     eye_color_question = input("What color are your eyes? : ")
     hair_color_question = input("What color is your hair")
     city_state_question = input("What city and state do you live in? :")
-    user_profile = (user_names, eye_color_question, hair_color_question, city_state_question)
-    user_profiles = []
-    user_profiles.append(user_profile)
-    print(user_profile)
+    user_name_data = (user_names)
+    user_profiles = {}
+    user_profiles.update({"Username":user_name_question, "Eye Color":eye_color_question, 
+    "Hair Color":hair_color_question, "City State":city_state_question})
+    print(user_name_data)
     print(user_profiles)
 #def user_profile(user_names, eye_color_question, hair_color_question, city_state_question):
     #user_name_question = input("Enter a username: ")
