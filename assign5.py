9eabcf76d4e323e94069783a970f9830a9edfaec
agraham102690 <grahamam1026@gmail.com>
2022-11-25 14:06:19 -0500

    added some more code

--- assign5.py
+++ assign5.py
@@ -1,13 +1,27 @@
 user_names = []
 eye_color = ("green", "blue", "brown", "hazel", "grey")
-print("Eye colors: " + eye_color)
+print("Eye colors: " + str(eye_color))
 hair_color = ("brown", "black", "blonde", "red" )
-print("Hair colors: " + hair_color)
-city_state = {"City":"State"}
-def user_profile():
-    user_name_question = input("Enter a username: ")
-    user_name_question.append(user_names)
-    eye_color_question = input("What color are your eyes? : ")
-    hair_color_qusestion = input("What color is your hair")
-    city_state_question = input("What city and state do you live in? :")
+print("Hair colors: " + str(hair_color))
+city_state = {"":""}
+while True:
 
+    user_name_question = input("Enter a username: ")
+    user_names.append(user_name_question)
+    eye_color_question = input("What color are your eyes? : ")
+    hair_color_question = input("What color is your hair")
+    city_state_question = input("What city and state do you live in? :")
+    user_profile = (user_names, eye_color_question, hair_color_question, city_state_question)
+    user_profiles = []
+    user_profiles.append(user_profile)
+    print(user_profile)
+    print(user_profiles)
+#def user_profile(user_names, eye_color_question, hair_color_question, city_state_question):
+    #user_name_question = input("Enter a username: ")
+   # user_names.append(user_name_question)
+    #eye_color_question = input("What color are your eyes? : ")
+    #hair_color_question = input("What color is your hair")
+    #city_state_question = input("What city and state do you live in? :")
+    #return user_profile(user_names, eye_color_question, hair_color_question, city_state_question)
+    
+#user_profile()
